6d63ca018368f0dd19f5b9b52a92bf5447c8ae71
Muhammad Arif Annaili Fitrawan-E41181232 <arlopaz.uye121299@gmail.com>
2020-05-01 21:32:47 +0700

    update detail

--- application/views/penyewa/index.php
+++ application/views/penyewa/index.php
@@ -24,7 +24,7 @@
                 </div>
             </div> -->
         </div>
-        <div class="carousel-item bg bg1">
+        <div class="carousel-item bg bg3">
             <!-- <div class="container">
                 <div class="carousel-caption">
                     <h1>One more for good measure.</h1>
@@ -84,7 +84,7 @@
             foreach ($kos as $tb) { ?>
                 <div class="col-lg-4 col-sm-6 mb-4">
                     <div class="portfolio-item">
-                        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                        <a class="portfolio-link" data-toggle="modal" href="#detailKos_<?= $tb['id_kos'] ?>">
                             <div class="portfolio-hover">
                                 <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
                             </div>
@@ -110,7 +110,7 @@
     <div class="container">
         <div class="text-center">
             <h2 class="section-heading text-uppercase">Tentang Kami</h2>
-            <h3 class="section-subheading text-white">Aplikasi ini adalah aplikai yang membantu mahasiswa untuk mencari tempat tinggal sementara khususnya mahasiswa jember</h3>
+            <h3 class="section-subheading text-white">Aplikasi ini adalah aplikasi yang membantu mahasiswa untuk mencari tempat tinggal sementara khususnya mahasiswa jember</h3>
         </div>
         <div class="row align-items-stretch mb-5">
             <div class="col-md-6">
@@ -142,3 +142,54 @@
         </div>
     </div>
 </section>
+
+<!-- Modal -->
+<?php
+foreach ($kos as $p) :
+    $id = $p['id_kos'];
+    $nama = $p['namakos'];
+    $foto = $p['fotokos'];
+    $khusus = $p['khususkos'];
+    $alamat = $p['alamatkos'];
+    $fasilitas = $p['fasilitaskos'];
+    $lingkungan = $p['lingkungankos'];
+    $peraturan = $p['peraturankos'];
+
+    $query_user = "SELECT * FROM datakos WHERE id_kos = $id";
+    $data_user = $this->db->query($query_user)->result_array();
+?>
+    <div class="portfolio-modal modal fade" id="detailKos_<?= $id; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg" /></div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <div class="modal-body">
+                                <!-- Project Details Go Here-->
+                                <h2 class="text-uppercase"><?= $nama; ?></h2>
+                                <p class="item-intro text-muted">Deskripsi tempat kos</p>
+                                <img class="img-fluid d-block mx-auto" src="<?= base_url(); ?>assets/img/foto_kos/<?= $foto; ?>" alt="" />
+                                <div class="text-uppercase">
+                                    <?php if ($khusus == 1) {
+                                        echo '<span class="badge badge-success">Putra</span>';
+                                    } else {
+                                        echo '<span class="badge badge-info">Putri</span>';
+                                    }  ?>
+                                </div>
+                                <p>Peraturan Kos: <?= $peraturan; ?></p>
+                                <ul class="list-inline">
+                                    <li>Fasilitas Kos: <?= $fasilitas; ?></li>
+                                    <li>Alamat: <?= $alamat; ?></li>
+                                    <li>Lingkungan: <?= $lingkungan; ?></li>
+                                </ul>
+                                <button class="btn btn-primary" data-dismiss="modal" type="button"><i class="fas fa-times mr-1"></i>Close Project</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php endforeach; ?>
